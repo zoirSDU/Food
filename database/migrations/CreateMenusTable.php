83bba15f4ae979c12faa68600b16c6bc6aacf395
sduit <kabirov.z@rahmetapp.kz>
2019-08-24 19:49:03 +0600

    Create menu page

---
+++ database/migrations/CreateMenusTable.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenusTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('product_id');
+            $table->unsignedInteger('product_type_id');
+            $table->string('status');
+            $table->string('cooking_date');
+            $table->string('delivery_itself');
+            $table->string('free_delivery_radius');
+            $table->string('free_delivery_amount');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('menus');
+    }
+}
